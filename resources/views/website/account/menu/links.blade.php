e9f00ff550d9a4ac8bd26a382c8bd225e4588556
dipankar kataki <dipankar.kataki@ekodusinc.com>
2024-03-04 16:10:42 +0530

    menu links fixed for dashboard

--- resources/views/website/account/menu/links.blade.php
+++ resources/views/website/account/menu/links.blade.php
@@ -1,5 +1,5 @@
 <ul class="account-nav">
-    <li><a href="{{route('website.account.myaccount')}}" class={{ Request::segment(3) == 'my-account' ? 'menu-link menu-link_us-s menu-link_active' : 'menu-link menu-link_us-s'}}>Dashboard</a></li>
+    <li><a href="{{route('website.account.myaccount')}}" class="{{ Request::segment(3) == 'my-account' ? 'menu-link menu-link_us-s menu-link_active' : 'menu-link menu-link_us-s'}}">Dashboard</a></li>
     <li><a href="{{route('website.account.myorders')}}" class="{{ Request::segment(3) == 'my-orders' ? 'menu-link menu-link_us-s menu-link_active' : 'menu-link menu-link_us-s'}}">Orders</a></li>
     <li><a href="{{route('website.account.myAddress')}}" class="{{ Request::segment(3) == 'my-address' ? 'menu-link menu-link_us-s menu-link_active' : 'menu-link menu-link_us-s'}}">Addresses</a></li>
     <li><a href="{{route('website.account.details')}}" class="{{ Request::segment(3) == 'details' ? 'menu-link menu-link_us-s menu-link_active' : 'menu-link menu-link_us-s'}}">Account Details</a></li>
