b26c88bad9c0e11c8de1b63973bed56c745d65da
dipankar kataki <dipankar.kataki@ekodusinc.com>
2024-03-11 12:54:57 +0530

    new photos added inside category image

--- resources/views/website/home/category-promotion/category-promotion.blade.php
+++ resources/views/website/home/category-promotion/category-promotion.blade.php
@@ -2,10 +2,10 @@
     <div class="row">
         <div class="col-lg-4">
             <div class="grid-banner__item position-relative mb-3">
-                <img loading="lazy" class="w-100 h-auto" src="{{asset('images/home/demo2/grid_banner_1.jpg')}}" width="450"
-                    height="450" alt="">
+                <img loading="lazy" class="w-100" src="{{asset('assets/Womensware/printed-dress-6.jpg')}}" width="450"
+                    height="450" style="object-fit:cover;object-position:top;" alt="Clothing Cover Image">
                 <div class="content_abs content_center text-center">
-                    <h3 class="text-uppercase fw-bold mb-1">CLOTHING</h3>
+                    <h3 class="text-uppercase fw-bold mb-1" style="text-shadow: 0px 0px 10px white;">CLOTHING</h3>
                     {{-- <h3 class="text-uppercase fw-bold mb-1">Horizons</h3> --}}
                     <a href="shop1.html" class="btn-link default-underline text-uppercase fw-medium">Shop Now</a>
                 </div><!-- /.content_abs .content_center -->
@@ -13,10 +13,10 @@
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
             <div class="grid-banner__item position-relative mb-3">
-                <img loading="lazy" class="w-100 h-auto" src="{{asset('images/home/demo2/grid_banner_2.jpg')}}" width="450"
-                    height="450" alt="">
+                <img loading="lazy" class="w-100" src="{{asset('assets/Accessories/jewel-1.jpg')}}" width="450"
+                    height="450" style="object-fit:cover;object-position:top;" alt="Accessories Cover Image">
                 <div class="content_abs content_center text-center">
-                    <h3 class="text-uppercase fw-bold mb-1">Accessories</h3>
+                    <h3 class="text-uppercase fw-bold mb-1" style="text-shadow: 0px 0px 10px white;">Accessories</h3>
                     {{-- <h3 class="text-uppercase fw-bold mb-1">Hats</h3> --}}
                     <a href="shop1.html" class="btn-link default-underline text-uppercase fw-medium">Shop Now</a>
                 </div><!-- /.content_abs .content_center -->
@@ -24,13 +24,13 @@
         </div><!-- /.col-lg-4 -->
         <div class="col-lg-4">
             <div class="grid-banner__item position-relative mb-3">
-                <img loading="lazy" class="w-100 h-auto" src="{{asset('images/home/demo2/grid_banner_3.jpg')}}" width="450"
-                    height="450" alt="">
+                <img loading="lazy" class="w-100" src="{{asset('assets/Homeware/bedsheet-5.jpeg')}}" width="450"
+                    height="450" style="object-fit:cover;object-position:top;" alt="Homeware Cover Image">
                 <div class="content_abs content_center text-center">
                     {{-- <p class="text-white mb-1">Free delivery for Nneels members</p> --}}
-                    <h3 class="text-uppercase fw-bold text-white mb-1">Homeware</h3>
+                    <h3 class="text-uppercase fw-bold mb-1" style="text-shadow: 0px 0px 10px white;">Homeware</h3>
                     {{-- <h3 class="text-uppercase fw-bold text-white mb-1">To 10% Off</h3> --}}
-                    <a href="shop1.html" class="btn-link default-underline text-uppercase text-white fw-medium">Shop Now</a>
+                    <a href="shop1.html" class="btn-link default-underline text-uppercase fw-medium">Shop Now</a>
                 </div><!-- /.content_abs .content_center -->
             </div>
         </div><!-- /.col-lg-4 -->
